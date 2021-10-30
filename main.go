3774b28eb1c1c0c003a2de3f469d93cdc4785474
Amrendra <amren1254@gmail.com>
2021-10-30 22:40:27 +0530

    update:large file download progress counter

--- main.go
+++ main.go
@@ -5,36 +5,73 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
+
+	humanize "github.com/dustin/go-humanize"
 )
 
-func main() {
-	fileUrl := "https://golangcode.com/logo.svg"
-	err := DownloadFile("logo.svg", fileUrl)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Downloaded: " + fileUrl)
+// WriteCounter counts the number of bytes written to it. It implements to the io.Writer interface
+// and we can pass this into io.TeeReader() which will report progress on each write cycle.
+type WriteCounter struct {
+	Total uint64
+}
+
+func (wc *WriteCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+	return n, nil
+}
+func (wc WriteCounter) PrintProgress() {
+	// Clear the line by using a character return to go back to the start and remove
+	// the remaining characters by filling it with spaces
+	fmt.Printf("\r%s", strings.Repeat(" ", 35))
+
+	// Return again and print current status of download
+	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
+	fmt.Printf("\rDownloading... %s complete", humanize.Bytes(wc.Total))
 }
 
 // DownloadFile will download a url to a local file. It's efficient because it will
 // write as it downloads and not load the whole file into memory.
 func DownloadFile(filepath string, url string) error {
-
+	// Create the file
+	out, err := os.Create(filepath + ".tmp")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
 	// Get the data
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
+	// Create our progress reporter and pass it to be used alongside our writer
+	counter := &WriteCounter{}
+	if _, err = io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
+		out.Close()
 		return err
 	}
-	defer out.Close()
+	// The progress use the same line so print a new line once it's finished downloading
+	fmt.Print("\n")
 
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
+	// Close the file without defer so it can happen before Rename()
+	out.Close()
+
+	if err = os.Rename(filepath+".tmp", filepath); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("Download Started")
+
+	fileUrl := "https://golangcode.com/logo.svg"
+	err := DownloadFile("logo.svg", fileUrl)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Downloaded: " + fileUrl)
 }
